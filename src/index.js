c5acef33227bc37828465e1e9be4225caa8b1244
Eylem Aytas <eylem.aytas@hotmail.com>
2023-04-18 15:34:16 -0400

    Modifies some files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-console.log('Write your code here');
+//console.log('Write your code here');
+const imageLocation = document.getElementById("exhibit-image");
+const exhibitDescription = document.getElementById("exhibit-description");
+const exhibitTitle = document.getElementById("exhibit-title");
+const commentsSection = document.getElementById("comments-section");
+const ticketsBought = document.getElementById("tickets-bought");
+const ticketsButton = document.getElementById("buy-tickets-button");
+let counter = 0
+fetch("http://localhost:3000/current-exhibits/1")
+  .then((response) => response.json())
+  .then((exhibit) => {
+    imageLocation.src = exhibit.image;
+    exhibitDescription.textContent = exhibit.description;
+    exhibitTitle.textContent = exhibit.title;
+    ticketsBought.textContent = `${exhibit.tickets_bought} tickets bought.`;
+    exhibit.comments.forEach((comment) => {
+      postComments(comment);
+    });
+  });
+
+function postComments(comment) {
+  const newComment = document.createElement("p");
+  newComment.textContent = comment;
+  commentsSection.appendChild(newComment);
+}
+
+const commentForm = document.getElementById("comment-form");
+commentForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const inputComment = event.target[0].value;
+  postComments(inputComment);
+});
+
+ticketsButton.addEventListener("click", ticketCounter)
+
+function ticketCounter() {
+  counter++;
+  ticketsBought.textContent = `${counter} tickets bought.`;
+}
